7dc9da655d727ce1049df0f476b31305325d729c
Lucy Li <luli@hsph.harvard.edu>
2018-04-20 11:38:38 -0400

    new lessons

---
+++ scripts/01-getting-started.R
@@ -0,0 +1,12 @@
+install.packages(c("swirl", "swirlify"))
+library(swirl)
+library(swirlify)
+new_lesson("00Introduction", "../../mcmc-infectious-disease-epi")
+#swirlify("00Introduction", "../../mcmc-infectious-disease-epi")
+
+new_lesson("01Theory", "../../mcmc-infectious-disease-epi")
+new_lesson("02WorkingExample", "../../mcmc-infectious-disease-epi")
+new_lesson("03Coding", "../../mcmc-infectious-disease-epi")
+new_lesson("04Troubleshooting", "../../mcmc-infectious-disease-epi")
+new_lesson("05FurtherReading", "../../mcmc-infectious-disease-epi")
+
