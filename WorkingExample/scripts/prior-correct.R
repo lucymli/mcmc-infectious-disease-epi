099743210ef6fc1bb790ae5b62e5a35e7e33e005
Lucy Li <luli@hsph.harvard.edu>
2018-05-07 15:36:21 -0400

    correct prior function

---
+++ WorkingExample/scripts/prior-correct.R
@@ -0,0 +1,6 @@
+flu_priors <- function (params) {
+  beta_prior <- dunif(params[["beta"]], min=0, max=10, log=TRUE)
+  gamma_prior <- dlnorm(1/params[["gamma"]], meanlog=1.8, sdlog=0.5, log=TRUE)
+  # Complete the following line
+  return (beta_prior+gamma_prior)
+}
