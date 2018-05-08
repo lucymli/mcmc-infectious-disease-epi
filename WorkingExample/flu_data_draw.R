494c03345a51f23cc2bff31b2673d6882520b716
Lucy Li <luli@hsph.harvard.edu>
2018-05-07 21:37:58 -0400

    delete

--- WorkingExample/flu_data_draw.R
+++
@@ -1,5 +0,0 @@
-ggplot(base_env$influenza_england_1978_school) + theme_classic() +
-  geom_point(aes(x=date, y=in_bed)) +
-  xlab("Date") +
-  ylab("In bed (prevalence)") +
-  scale_x_date(date_labels="%b %d", date_breaks="2 days")
